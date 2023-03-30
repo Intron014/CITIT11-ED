e94081bb7936234aef38e8649de19152a7df80a7
Jorge Benjumea Burillo <me@intron014.com>
2023-03-30 15:51:12 +0200

    Comenzamos la práctica!

--- ED - Practica 3/listaordinal/PruebasLista.java
+++ ED - Practica 3/listaordinal/PruebasLista.java
@@ -2,6 +2,14 @@ package listaordinal;
 
 public class PruebasLista {
     public static void main(String[] args) {
-        // Introducir aquí las pruebas pedidas en la práctica
+        Producto mesa_escritorio = new Producto("Mesa escritorio", 185, 2);
+        Producto silla = new Producto("Silla oficina", 95.9f, 3);
+        Producto mesa_cocina = new Producto("Mesa cocina", 125, 1);
+        Producto sillon = new Producto("Sillón Reclinable", 230, 2);
+
+        ListaOrdinal listosa = new ListaOrdinal();
+
+
+
     }
 }
