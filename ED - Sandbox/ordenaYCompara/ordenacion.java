de2b8dfc7735e852373cfcb112ac0875f6a3efa5
Jorge Benjumea Burillo <me@intron014.com>
2023-04-13 11:37:01 +0200

    Ordenacion is a go! (Using Java libraries)

---
+++ ED - Sandbox/ordenaYCompara/ordenacion.java
@@ -0,0 +1,22 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ordenacion {
+    public static void main(String[] args){
+        LinkedList<Integer> lista1 = new LinkedList<Integer>();
+        lista1.add(1); lista1.add(2); lista1.add(3); lista1.add(4); lista1.add(5); lista1.add(6); lista1.add(7);
+        LinkedList<Integer> lista2 = new LinkedList<Integer>();
+        lista2.add(1); lista2.add(3); lista2.add(5); lista2.add(7);
+        Iterator<Integer> it1 = lista1.listIterator();
+
+        System.out.printf("Items comunes: ");
+        while(it1.hasNext()){
+            Integer one = it1.next();
+            if(lista2.contains(one)){
+                System.out.printf(String.valueOf(one));
+                System.out.printf(" ");
+            }
+            }
+        }
+
+    }
