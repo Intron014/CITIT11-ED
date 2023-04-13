60286982e6e2fbfb2462533576d9bbbda8aef88d
Jorge Benjumea Burillo <me@intron014.com>
2023-04-13 11:40:02 +0200

    One can dream... right?

--- ED - Sandbox/ordenaYCompara/ordenacion.java
+++ ED - Sandbox/ordenaYCompara/ordenacion.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 public class ordenacion {
     public static void main(String[] args){
         LinkedList<Integer> lista1 = new LinkedList<Integer>();
+        //If only I knew how to make the system read input...
         lista1.add(1); lista1.add(2); lista1.add(3); lista1.add(4); lista1.add(5); lista1.add(6); lista1.add(7);
         LinkedList<Integer> lista2 = new LinkedList<Integer>();
         lista2.add(1); lista2.add(3); lista2.add(5); lista2.add(7);
