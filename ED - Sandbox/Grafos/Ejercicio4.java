f63197f06f4d23c150903c97b1f3e7f8ee6b9c34
Jorge Benjumea Burillo <me@intron014.com>
2023-06-06 15:53:26 +0200

    Ejercicios Grafos

---
+++ ED - Sandbox/Grafos/Ejercicio4.java
@@ -0,0 +1,6 @@
+public class Ejercicio4 {
+    public int Ejercicio4(int n){
+        n = (n*(n-1)/2);
+        return n;
+    }
+}
--- ED - Sandbox/Grafos/receptivo.java
+++
@@ -1,3 +0,0 @@
-// Nos vemos toda la fila y en cuanto encontremos una fila a todo True
-// devolvemos ese valor
-public
